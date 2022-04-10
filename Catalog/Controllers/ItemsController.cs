650e8ff8cfab6ef341c3a83b60a9bdb75219e75e
Tim Whelan <tim.whelan@citywonders.com>
2022-04-10 09:35:56 +0100

    add methods for Deleting an item

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -75,5 +75,20 @@ namespace Catalog.Controllers
 
             return NoContent();
         }
+
+        // DELETE /items/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = _repository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            _repository.DeleteItem(id);
+
+            return NoContent();
+        }
     }
 }
--- Catalog/Repositiories/IItemsRepository.cs
+++ Catalog/Repositiories/IItemsRepository.cs
@@ -8,5 +8,6 @@ namespace Catalog.Repositories
         IEnumerable<Item> GetItems();
         void CreateItem(Item item);
         void UpdateItem(Item item);
+        void DeleteItem(Guid id);
     }
 }
